b30dd247f8c3541ad8d1edfee1c1c3862a110308
Jobayrul Hasan <jobayerdpi11@gmail.com>
2025-07-06 16:45:36 +0600

    last changes

--- dailypractice/dailypractice/urls.py
+++ dailypractice/dailypractice/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),  # Include the home app's URLs
+    path('about/', include('about.urls')),  # Include the dailypractice app's URLs
+    path('skills/', include('skills.urls')),  # Include the dailypractice app's URLs
+    path('blog/', include('blog.urls')),  # Include the contact app's URLs
 ]
